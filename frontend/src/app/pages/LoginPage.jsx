81c259b2e6e0ebab976864c9ef6a641196d6e44a
Omkar-Pandurang-Jadhav <omkarj0710@gmail.com>
2026-05-04 13:41:55 +0530

    test webhook

--- frontend/src/app/pages/LoginPage.jsx
+++ frontend/src/app/pages/LoginPage.jsx
@@ -29,7 +29,7 @@ export function LoginPage() {
     try {
       if (isLogin) {
         await login(email, password);
-        toast.success('Welcome Back!');
+        toast.success('Welcome Back Bhai!');
       } else {
         await register(email, password, name, role);
         toast.success('Account created successfully!');
